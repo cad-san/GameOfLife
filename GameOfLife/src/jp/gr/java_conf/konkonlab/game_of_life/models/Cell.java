6c13a16dabf3970df74c8367974dc8e24feddbee
cad-san <cadsan1986@gmail.com>
2012-03-19 02:02:07 +0900

    fix wrong indent

--- GameOfLife/src/jp/gr/java_conf/konkonlab/game_of_life/models/Cell.java
+++ GameOfLife/src/jp/gr/java_conf/konkonlab/game_of_life/models/Cell.java
@@ -1,33 +1,33 @@
 package jp.gr.java_conf.konkonlab.game_of_life.models;
 
 public class Cell {
-		public static final String AliveCell = "ALIVE";
-		public static final String DeadCell  = "DEAD";
+	public static final String AliveCell = "ALIVE";
+	public static final String DeadCell  = "DEAD";
 
-		private String status = DeadCell;
-		private int group;
-		
-		public Cell(String status)
-		{
-			this.status = status;
-		}
-		
-		@Override
-		public String toString() {
-			return status;
-		}
-		
-		@Override
-		public boolean equals(Object another) {
-		return status.equals(another);
-		}
-
-		public void setGroup(int group) {
-			this.group = group;
-		}
-
-		public int getGroup() {
-			return group;
-		}
+	private String status = DeadCell;
+	private int group;
+	
+	public Cell(String status)
+	{
+		this.status = status;
+	}
+	
+	@Override
+	public String toString() {
+		return status;
+	}
+	
+	@Override
+	public boolean equals(Object another) {
+	return status.equals(another);
+	}
+	
+	public void setGroup(int group) {
+		this.group = group;
+	}
+	
+	public int getGroup() {
+		return group;
+	}
 
 }
