08962b85656f7b194a8c0fc4208f3a45012b2cc4
cad-san <cadsan1986@gmail.com>
2012-03-19 02:35:10 +0900

    add setup() in CellTest to remove duplication

--- GameOfLifeTest/src/jp/gr/java_conf/konkonlab/game_of_life/models/test/CellTest.java
+++ GameOfLifeTest/src/jp/gr/java_conf/konkonlab/game_of_life/models/test/CellTest.java
@@ -5,26 +5,33 @@ import junit.framework.TestCase;
 
 public class CellTest extends TestCase {
 
+	Cell deadCell;
+	Cell aliveCell;
+	
+	@Override
+	protected void setUp() throws Exception {
+		deadCell = new Cell(Cell.DeadCell);
+		aliveCell = new Cell(Cell.AliveCell);
+		super.setUp();
+	}
+	
 	public void testInitAlive() throws Exception {
-		Cell cell = new Cell(Cell.AliveCell);
-		assertEquals("ALIVE", cell.toString());
-		assertTrue(cell.equals(Cell.AliveCell));
+		assertEquals("ALIVE", aliveCell.toString());
+		assertTrue(aliveCell.equals(Cell.AliveCell));
 	}
 
 	public void testInitDead() throws Exception {
-		Cell cell = new Cell(Cell.DeadCell);
-		assertEquals("DEAD", cell.toString());
-		assertTrue(cell.equals(Cell.DeadCell));
+		assertEquals("DEAD", deadCell.toString());
+		assertTrue(deadCell.equals(Cell.DeadCell));
 	}
 	
 	public void testGroupArive() throws Exception {
-		Cell cell = new Cell(Cell.AliveCell);
-		cell.setGroup(1);
-		assertEquals(1, cell.getGroup());
+		aliveCell.setGroup(1);
+		assertEquals(1, aliveCell.getGroup());
 	}
 	
 	public void testGroupDead() throws Exception {
-		Cell cell = new Cell(Cell.DeadCell);
-		assertEquals(0, cell.getGroup());
+		deadCell.setGroup(1); // ignored
+		assertEquals(0, deadCell.getGroup());
 	}
 }
