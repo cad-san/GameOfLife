0a69ab33118af0de0dd656cb172b1705a2908559
cad-san <cadsan1986@gmail.com>
2012-03-20 22:53:44 +0900

    refactored GridTest class

--- GameOfLifeTest/src/jp/gr/java_conf/konkonlab/game_of_life/models/test/GridTest.java
+++ GameOfLifeTest/src/jp/gr/java_conf/konkonlab/game_of_life/models/test/GridTest.java
@@ -8,6 +8,32 @@ import java.util.List;
 
 public class GridTest extends TestCase {
 
+	private Grid createGrid(int[][] templete, int numX, int numY) {
+		List<List<Boolean>> initMatrix = new ArrayList<List<Boolean>>();
+
+		for(int y = 0; y < numX; y++) {
+			initMatrix.add(new ArrayList<Boolean>());
+			for(int x = 0; x < numY; x++){
+				initMatrix.get(y).add(templete[y][x] == 1);
+			}
+		}
+		
+		return new Grid(initMatrix);
+	}
+	
+	private void judgeExprectedGrid(Grid actualGrid, int[][] expectMatrix) throws Exception {
+		for(int x = 0; x < actualGrid.getNumX(); x++) {
+			for(int y = 0; y < actualGrid.getNumY(); y++) {
+				if( expectMatrix[y][x] == 1) {
+					assertTrue(actualGrid.getCellAt(x,y).isAlive());
+				}
+				else {
+					assertFalse(actualGrid.getCellAt(x,y).isAlive());
+				}
+			}
+		}
+	}
+	
 	public void testFullyInit() throws Exception {
 		int numX = 5;
 		int numY = 10;
@@ -36,27 +62,8 @@ public class GridTest extends TestCase {
 				{0, 0, 0, 0, 0},
 		};
 		
-		List<List<Boolean>> cellMatrix = new ArrayList<List<Boolean>>();
-		
-		for(int y = 0; y < numX; y++) {
-			cellMatrix.add(new ArrayList<Boolean>());
-			for(int x = 0; x < numY; x++){
-				cellMatrix.get(y).add(initMatix[y][x] == 1);
-			}
-		}
-		
-		Grid grid = new Grid(cellMatrix);
+		Grid grid = createGrid(initMatix, numX, numY);
 
-		for(int x = 0; x < numX; x++) {
-			for(int y = 0; y < numY; y++) {
-				if( initMatix[y][x] == 1) {
-					assertTrue(grid.getCellAt(x,y).isAlive());
-				}
-				else {
-					assertFalse(grid.getCellAt(x,y).isAlive());
-				}
-			}
-		}
-
+		judgeExprectedGrid(grid, initMatix);
 	}
 }
