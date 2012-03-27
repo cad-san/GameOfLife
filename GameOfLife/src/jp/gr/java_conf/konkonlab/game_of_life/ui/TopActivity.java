6962596c0f406ca78ad79d416073b4125f27e323
cad-san <cadsan1986@gmail.com>
2012-03-28 01:15:18 +0900

    change target and use ActionBarSherlock

--- GameOfLife/src/jp/gr/java_conf/konkonlab/game_of_life/ui/TopActivity.java
+++ GameOfLife/src/jp/gr/java_conf/konkonlab/game_of_life/ui/TopActivity.java
@@ -1,14 +1,15 @@
 package jp.gr.java_conf.konkonlab.game_of_life.ui;
 
 import jp.gr.java_conf.konkonlab.game_of_life.R;
-import android.app.Activity;
 import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
 
-public class TopActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+public class TopActivity extends SherlockActivity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.Theme_Sherlock);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
     }
 }
