2a6a2f1f64c6375a6e00dd2cce4a685a59093174
cad-san <cadsan1986@gmail.com>
2012-04-06 23:14:40 +0900

    add LifeGridAxis class for calculation of Grid Coordinate

---
+++ GameOfLife/src/jp/gr/java_conf/konkonlab/game_of_life/ui/LifeGridAxis.java
@@ -0,0 +1,73 @@
+package jp.gr.java_conf.konkonlab.game_of_life.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LifeGridAxis {
+	int gridWidth;
+	int gridHeight;
+	int lineWeight;
+	
+	List<Integer> lineXAxis;
+	List<Integer> lineYAxis;
+
+	public LifeGridAxis(int numCellX, int numCellY, int lineWeight, int gridWidth, int gridHeight) {
+		this.gridWidth = gridWidth;
+		this.gridHeight = gridHeight;
+		this.lineWeight = lineWeight;
+		lineXAxis = createAxis(numCellX, lineWeight, gridWidth);
+		lineYAxis = createAxis(numCellY, lineWeight, gridHeight);
+	}
+	
+	public int getLineXAxisAt(int x) {
+		return lineXAxis.get(x);
+	}
+	
+	public int getLineYAxisAt(int y) {
+		return lineYAxis.get(y);
+	}
+
+	public int getNumLineX() {
+		return lineXAxis.size();
+	}
+
+	public int getNumLineY() {
+		return lineYAxis.size();
+	}
+	
+	public int getCellXAxisAt(int x) {
+		return lineXAxis.get(x) + lineWeight;
+	}
+
+	public int getCellYAxisAt(int y) {
+		return lineYAxis.get(y) + lineWeight;
+	}
+	
+	public int getNumCellX() {
+		return lineXAxis.size() - 1;
+	}
+
+	public int getNumCellY() {
+		return lineYAxis.size() - 1;
+	}
+	
+	private List<Integer> createAxis(int numOfCell, int lineWeight, int gridBreadth) {
+		List<Integer> cellCorrd = new ArrayList<Integer>();
+
+		float cellBreadth = calcCellBreadth(numOfCell, lineWeight, gridBreadth);
+		
+		for(int i = 0; i <= numOfCell; i++) {
+			cellCorrd.add( calcCellDimension(cellBreadth, i, lineWeight));
+		}
+		
+		return cellCorrd;
+	}
+	
+	private int calcCellDimension(float cellBreadth, int cellNo, int lineWeight) {
+		return (int)(cellNo*(cellBreadth+lineWeight));
+	}
+
+	private float calcCellBreadth(int numCell, int lineWeight, int areaBreadth) {
+		return (float)(areaBreadth - (numCell + 1) * lineWeight ) / numCell;
+	}
+}
---
+++ GameOfLifeTest/src/jp/gr/java_conf/konkonlab/game_of_life/ui/test/LifeGridAxisTest.java
@@ -0,0 +1,74 @@
+package jp.gr.java_conf.konkonlab.game_of_life.ui.test;
+
+import jp.gr.java_conf.konkonlab.game_of_life.ui.LifeGridAxis;
+import junit.framework.TestCase;
+
+public class LifeGridAxisTest extends TestCase {
+
+	public void testOneCell() throws Exception {
+		int numCellX = 1;
+		int numCellY = 1;
+		int lineWeight = 1;
+		int gridWidth = 3;
+		int gridHeight = 3;
+		LifeGridAxis gridAxis = new LifeGridAxis(numCellX, numCellY, lineWeight, gridWidth, gridHeight);
+		
+		assertNumber(gridAxis, numCellX, numCellY);
+
+		assertXAxis(gridAxis, 1, lineWeight);
+		assertYAxis(gridAxis, 1, lineWeight);
+	}
+
+	public void testGrid2by2() throws Exception {
+		int numCellX = 2;
+		int numCellY = 2;
+		int lineWeight = 1;
+		int gridWidth = 7;
+		int gridHeight = 7;
+		LifeGridAxis gridAxis = new LifeGridAxis(numCellX, numCellY, lineWeight, gridWidth, gridHeight);
+
+		assertNumber(gridAxis, numCellX, numCellY);
+		
+		assertXAxis(gridAxis, 2, lineWeight);
+		assertYAxis(gridAxis, 2, lineWeight);
+	}
+	
+	public void testGrid2by3() throws Exception {
+		int numCellX = 2;
+		int numCellY = 3;
+		int lineWeight = 1;
+		int gridWidth = 7;
+		int gridHeight = 10;
+		LifeGridAxis gridAxis = new LifeGridAxis(numCellX, numCellY, lineWeight, gridWidth, gridHeight);
+
+		assertNumber(gridAxis, numCellX, numCellY);
+
+		assertXAxis(gridAxis, 2, lineWeight);
+		assertYAxis(gridAxis, 2, lineWeight);
+	}
+	
+	private void assertNumber(LifeGridAxis gridAxis, int numCellX, int numCellY) {
+		assertEquals(numCellX, gridAxis.getNumCellX());
+		assertEquals(numCellY, gridAxis.getNumCellY());
+		assertEquals(numCellX+1, gridAxis.getNumLineX());
+		assertEquals(numCellY+1, gridAxis.getNumLineY());
+	}
+	
+	private void assertXAxis(LifeGridAxis gridAxis, int cellWeight, int lineWeight) {
+		for(int x = 0; x < gridAxis.getNumLineX(); x++) {
+			assertEquals(x*(lineWeight+cellWeight), gridAxis.getLineXAxisAt(x));
+		}
+		for(int x = 0; x < gridAxis.getNumCellX(); x++) {
+			assertEquals(lineWeight * (x + 1) + cellWeight * x, cellWeight, gridAxis.getCellXAxisAt(x));
+		}
+	}
+	
+	private void assertYAxis(LifeGridAxis gridAxis, int cellWeight, int lineWeight) {
+		for(int y = 0; y < gridAxis.getNumLineY(); y++) {
+			assertEquals(y*(lineWeight+cellWeight), gridAxis.getLineYAxisAt(y));
+		}
+		for(int y = 0; y < gridAxis.getNumCellY(); y++) {
+			assertEquals(lineWeight * (y + 1) + cellWeight * y, cellWeight, gridAxis.getCellYAxisAt(y));
+		}
+	}
+}
